c4b054f9f1ee7b9be0d8d816bfbee6ba2ad6a4ea
Wenxing Zhao <i@zaxh.org>
2026-08-11 19:27:03 +0800

    chore: update Mitori cask to 0.2.1

--- Casks/mitori.rb
+++ Casks/mitori.rb
@@ -1,13 +1,13 @@
 cask "mitori" do
-  version "0.2.0"
-  sha256 "d0c75fa09b77e07fa18321de775d5fa080c29747b548e278dd563be0ce7b9cc0"
+  version "0.2.1"
+  sha256 "baca484a6ebfe7495ba3614732c2894bd91617a86e1dc52a5a0d746a0b788371"
 
   url "https://github.com/Zach677/mitori/releases/download/v#{version}/Mitori-#{version}.dmg"
   name "Mitori"
   desc "Monitor Apple ID store credit balances from the menu bar"
   homepage "https://github.com/Zach677/mitori"
 
-  depends_on macos: :tahoe
+  depends_on macos: :sonoma
 
   app "Mitori.app"
 
